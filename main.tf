174e95bfa9a6fbc577c4aa3628d1c10c7219aebb
vsgali <galisatish74@gmail.com>
2023-05-08 18:02:47 +0530

    added rg1 rg2 removed rg3

--- main.tf
+++ main.tf
@@ -11,5 +11,9 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+    subscription_id="df387313-b5f2-429f-8227-e142fc8c6b7b"
+    client_id="17c9ba3b-282e-4e2a-b4ae-608c009969ef"
+    client_secret="EaP8Q~Vg84DRUCeIMvrx9tVgDVhxnPxknEmVgapU"
+    tenant_id="ffab9f59-99fa-4165-96f0-2a6e4b9c46c5"
   features {}
 }
--- resource_group.tf
+++ resource_group.tf
@@ -7,8 +7,3 @@ resource "azurerm_resource_group" "rg2" {
     name = "devopsb27rg2"
     location = "westus"
 }
-
-resource "azurerm_resource_group" "rg3" {
-    name = "devopsb27rg3"
-    location = "eastus2"
-}
