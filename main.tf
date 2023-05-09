f68d9f7754fd2ce7d353a8eafd479edf2540a018
vsgali <galisatish74@gmail.com>
2023-05-09 12:11:53 +0530

    script

--- main.tf
+++ main.tf
@@ -14,15 +14,15 @@ terraform{
     storage_account_name = "devopsb27terraformproduc"
     container_name = "tfstate"
     key = "devopsb27-git-class.tfstate"
-    access_key = "Nm2IavBv6fBvLKOq6fb1lPuAKbfc8aQ7FU4q83mP7+aQFy1Uzo8y9v3j/VbXqP4BlPXp7Gcjhtsa+AStYebygg=="
+   # access_key = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
     }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-    subscription_id="df387313-b5f2-429f-8227-e142fc8c6b7b"
-    client_id="17c9ba3b-282e-4e2a-b4ae-608c009969ef"
-    client_secret="EaP8Q~Vg84DRUCeIMvrx9tVgDVhxnPxknEmVgapU"
-    tenant_id="ffab9f59-99fa-4165-96f0-2a6e4b9c46c5"
+   # subscription_id="aaaaaaaaaaaaaaaaaaaa"
+   # client_id="bbbbbbbbbbbbbbbbbbbb"
+   # client_secret="cccccccccccccccccccccccccc"
+   # tenant_id="ddddddddddddddddddd"
   features {}
 }
--- resource_group.tf
+++ resource_group.tf
@@ -1,14 +1,4 @@
-resource "azurerm_resource_group" "rg1" {
-    name = "devopsb27rg1"
+resource "azurerm_resource_group" "rg5" {
+    name = "devopsb27rg5"
     location = "eastus"
-}
-
-resource "azurerm_resource_group" "rg2" {
-    name = "devopsb27rg2"
-    location = "westus"
-}
-
-resource "azurerm_resource_group" "rg3" {
-    name = "devopsb27rg3"
-    location = "westus"
 }
